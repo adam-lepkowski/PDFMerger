895216af7f2e0d424df3bd461a590d039eb54623
adam <adam.lepkowski@hotmail.com>
2022-12-20 22:16:00 +0100

    Add event listener and row creation

---
+++ merger/static/merger/index.js
@@ -0,0 +1,37 @@
+var input = document.querySelector(".file-input");
+
+input.addEventListener("change", addNextInput);
+
+function addNextInput() {
+    var value = this.value;
+    var label = this.previousElementSibling;
+    var value = value.split("\\")[value.split("\\").length - 1];
+    if (value != "") {
+        label.textContent = value.split("\\")[value.split("\\").length - 1];
+        var parent = this.parentElement.parentElement;
+        createInputDiv(parent);
+    } else {
+        label.textContent = "Select file";
+    }
+}
+
+function createInputDiv(parent) {
+    var uploadsAmount = document.querySelectorAll("input").length;
+    if (uploadsAmount < 5) {
+        var inputId = "file-upload" + uploadsAmount.toString();
+        var div = document.createElement("div");
+        div.classList.add("file-row");
+        var label = document.createElement("label");
+        label.setAttribute("for", inputId);
+        var text = document.createTextNode("Select file");
+        label.appendChild(text);
+        var input = document.createElement("input");
+        input.setAttribute("type", "file");
+        input.setAttribute("id", inputId);
+        input.setAttribute("accept", ".pdf");
+        input.addEventListener("change", addNextInput);
+        div.appendChild(label);
+        div.appendChild(input);
+        parent.appendChild(div);
+    }
+}
