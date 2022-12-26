3a6440051a5f405ad554501a96f313dc9bb1056e
adam <adam.lepkowski@hotmail.com>
2022-12-26 10:09:43 +0100

    IndexView docstrings

--- merger/views.py
+++ merger/views.py
@@ -3,6 +3,12 @@ from django.views import View
 
 
 class IndexView(View):
+    """
+    Represent main page.
+    """
 
     def get(self, request):
+        """
+        Display main page.
+        """
         return render(request, "merger/index.html")
