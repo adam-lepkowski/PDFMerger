7d3ba8db078ee21e8bbd6a075724cb7b2299930c
adam <adam.lepkowski@hotmail.com>
2022-12-26 14:36:08 +0100

    Add PdfModel

--- merger/models.py
+++ merger/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.files.base import ContentFile
 
-# Create your models here.
+
+class PdfModel(models.Model):
+    """
+    Represent and store merged PDF files.
+    """
+
+    file = models.FileField(upload_to="pdfs")
+
+    def save(self, *args, **kwargs):
+        """
+        Convert BytesIO into ContentFile before saving.
+        """
+
+        self.file = ContentFile(self.file, "upload.pdf")
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return str(self.file.name)
