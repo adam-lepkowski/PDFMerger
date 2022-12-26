2eb9809fc5708f6c94df369ba61456505b385cbc
adam <adam.lepkowski@hotmail.com>
2022-12-26 14:37:36 +0100

    Add IndexView post method

--- merger/views.py
+++ merger/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django.core.files.base import ContentFile
+
+from .models import PdfModel
+from .utils import merge
 
 
 class IndexView(View):
@@ -11,4 +15,17 @@ class IndexView(View):
         """
         Display main page.
         """
+
+        return render(request, "merger/index.html")
+
+    def post(self, request):
+        """
+        Merge sent files and redirect to download page.
+        """
+
+        files = [request.FILES[file] for file in request.FILES]
+        merged = merge(files)
+        pdf = PdfModel(file=merged.getvalue())
+        pdf.save()
+        merged.close()
         return render(request, "merger/index.html")
