57232b5a0aa23049c9b91f1ee22fc70ef17a48d2
adam <adam.lepkowski@hotmail.com>
2022-12-28 20:11:32 +0100

    Name download path

--- merger/urls.py
+++ merger/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view()),
-    path("download/", views.DownloadView.as_view())
+    path("download/", views.DownloadView.as_view(), name="download")
 ]
