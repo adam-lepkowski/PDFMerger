32c39bee7034a9e353d11f21391b19ab17d79cd4
adam <adam.lepkowski@hotmail.com>
2022-12-29 12:23:24 +0100

    Refactor index post test

--- merger/tests/test_index.py
+++ merger/tests/test_index.py
@@ -1,5 +1,5 @@
 from io import BytesIO
-from unittest.mock import patch
+from unittest.mock import patch, call, MagicMock
 
 from django.test import TestCase
 
@@ -20,15 +20,14 @@ class TestIndex(TestCase):
         response = self.client.get("/")
         self.assertEqual(response.resolver_match.func.view_class, IndexView)
 
-    @patch("merger.views.PdfModel.save")
+    @patch("merger.views.PdfModel", autospec=True)
     @patch("merger.views.merge", return_value=BytesIO(b"merged_content"))
-    def test_index_post(self, merge_mock, save_mock):
+    def test_index_post(self, merge_mock, pdfmodel_mock):
         uploaded_file = BytesIO(b"uploaded_content")
         response = self.client.post("/", {"file-upload": uploaded_file})
         merge_mock.assert_called_once()
-        save_mock.assert_called_once()
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "merger/index.html")
-        self.assertEqual(response.resolver_match.func.view_class, IndexView)
+        self.assertTrue(call().save() in pdfmodel_mock.method_calls)
+        self.assertTrue(302, response.status_code)
+        self.assertTrue(response.url.startswith("/download/"))
         uploaded_file.close()
         merge_mock.return_value.close()
